70d02c845c893fba52a568719bdf903dbb00bea9
Alicia <amonmar709@g.educaand.es>
2024-04-08 00:04:43 +0200

    Correcion fallos

--- src/test/java/org/iesvdm/UtilesArraysTest.java
+++ src/test/java/org/iesvdm/UtilesArraysTest.java
@@ -7,6 +7,7 @@ import java.io.PrintStream;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UtilesArraysTest {
 
@@ -14,53 +15,55 @@ public class UtilesArraysTest {
     void limpiar(){
         //When
         //Partiendo de un array
-        int tabla[] = {9, 0, 1, 8, 7, 6, 5, 4, 3, 2};
         //do
-        UtilesArrays.limpiar();
+        int[] tabla = UtilesArrays.limpiar();
         //Compruebo que esta vacio
-        assertThat(tabla.length == 0);
+        assertThat(tabla.length).isEqualTo(0);
     }
 
     @Test
     void rellenarConCeros(){
         //When
-        int tabla[] = {1,2,3};
+        int[] nuevo = {};
+        int[] tabla = {1,2,3};
         int numeroElementos = 3;
         int posicion = 1;
 
         //do
-        UtilesArrays.rellenar(tabla, numeroElementos);
+        tabla = UtilesArrays.rellenar(nuevo, numeroElementos);
 
         //Compueblo que la longitud del array es igual al numero de elementos
-        assertThat( tabla.length == numeroElementos);
+        assertThat( tabla.length).isEqualTo(numeroElementos);
         //Compruebo que en una posición aleatoria el elemento sea cero
-        assertThat(tabla[posicion] == 0);
+        assertThat(tabla[posicion] ==0).isTrue();
     }
 
     @Test
     void rellenar(){
         //When
-        int tabla[] = {};
+        int[] nuevo = {};
+        int[] tabla = {1,2,3};
         int numeroElementos = 3;
+        int valor = 3;
 
         //do
-        UtilesArrays.rellenar(tabla, numeroElementos);
+        nuevo = UtilesArrays.rellenar(tabla, numeroElementos, valor);
 
         //Compueblo que la longitud del array es igual al numero de elementos
-        assertThat( tabla.length == numeroElementos);
+        assertThat( tabla.length).isEqualTo(valor);
     }
 
     @Test
     void insertarAlFinal(){
         //When
-        int tabla[] = {1, 2, 3, 4, 5};
-        int elemento = 6;
+        int[] tabla = {1, 2, 3, 4, 5};
+        int elemento = 5;
 
         //do
-        UtilesArrays.insertarAlFinal(tabla, elemento);
+       tabla = UtilesArrays.insertarAlFinal(tabla, elemento);
 
         //Then
-        assertThat(tabla.length-1 == elemento);
+        assertThat(tabla.length-1 == elemento).isTrue();
     }
 
     @Test
@@ -70,10 +73,10 @@ public class UtilesArraysTest {
         int elemento = 1;
 
         //do
-        UtilesArrays.insertarAlPrincipio(tabla, elemento);
+        tabla = UtilesArrays.insertarAlPrincipio(tabla, elemento);
 
         //Then
-        assertThat(tabla[0] == elemento);
+        assertThat(tabla[0]).isEqualTo(elemento);
     }
 
     @Test
@@ -84,10 +87,10 @@ public class UtilesArraysTest {
         int posicion = 2;
 
         //do
-        UtilesArrays.insertarEnPosicion(tabla, elemento, posicion);
+        tabla = UtilesArrays.insertarEnPosicion(tabla, elemento, posicion);
 
         //Then
-        assertThat(tabla[posicion] == elemento);
+        assertThat(tabla[posicion] !=elemento).isFalse();
     }
 
     @Test
@@ -98,10 +101,10 @@ public class UtilesArraysTest {
         int posicion = 1;
 
         //do
-        UtilesArrays.insertarOrdenado(tabla, elemento);
+        tabla = UtilesArrays.insertarOrdenado(tabla, elemento);
 
         //Then
-        assertThat(tabla[posicion] == elemento);
+        assertThat(tabla[posicion]).isEqualTo(elemento);
 
     }
 
@@ -111,11 +114,11 @@ public class UtilesArraysTest {
         int[] tabla = {1, 2, 3, 4, 5};
 
         //do
-        UtilesArrays.eliminarUltimo(tabla);
+        tabla = UtilesArrays.eliminarUltimo(tabla);
 
         //Then
         //Comprobamos que ahora el ultimo elemento es el cuatro
-        assertThat(tabla[tabla.length-1] == 4 );
+        assertThat(tabla[tabla.length-1] == 4 ).isTrue();
     }
 
 
@@ -125,11 +128,11 @@ public class UtilesArraysTest {
         int[] tabla = {1, 2, 3, 4, 5};
 
         //do
-        UtilesArrays.eliminarPrimero(tabla);
+        tabla = UtilesArrays.eliminarPrimero(tabla);
 
         //Then
         //Comprobamos que ahora el primer elemento es dos
-        assertThat(tabla[0] == 2);
+        assertThat(tabla[0] == 2).isTrue();
     }
 
     @Test
@@ -139,11 +142,11 @@ public class UtilesArraysTest {
         int posicion = 1;
 
         //do
-        UtilesArrays.eliminarPosicion(tabla, posicion);
+        tabla = UtilesArrays.eliminarPosicion(tabla, posicion);
 
         //Then
         //Comprobamos que ahora en la posicion 1 hay un 3
-        assertThat(tabla[posicion] == 3);
+        assertThat(tabla[posicion]).isEqualTo(3);
     }
 
     @Test
@@ -153,11 +156,11 @@ public class UtilesArraysTest {
         int elemento = 6;
 
         //do
-        UtilesArrays.eliminar(tabla, elemento);
+        tabla = UtilesArrays.eliminar(tabla, elemento);
 
         //Then
         //Comprobamos que en la ultima posicion ahora esta el 5
-        assertThat(tabla[tabla.length-1] == 5);
+        assertThat(tabla[tabla.length-1] == 5).isTrue();
     }
 
     @Test
@@ -166,13 +169,13 @@ public class UtilesArraysTest {
         int[] tabla = {6, 4, 5, 3, 2, 1};
 
         //do
-        UtilesArrays.ordenar(tabla);
+        tabla = UtilesArrays.ordenar(tabla);
 
         //Then
         //Comprobamos que ahora en la primera posicion esta el 1 y en la ultima
         //esta el 6
-        assertThat(tabla[0] == 1);
-        assertThat(tabla[tabla.length-1] == 6);
+        assertThat(tabla[0]).isEqualTo(1);
+        assertThat(tabla[tabla.length-1] == 6).isTrue();
     }
 
     @Test
@@ -185,9 +188,9 @@ public class UtilesArraysTest {
 
         //Then
         //Comprobamos varias posiciones para ver que han cambiado
-        assertThat(tabla[0] != 1);
-        assertThat(tabla[1] != 2);
-        assertThat(tabla[tabla.length-1] != 6);
+        assertThat(tabla[0]).isNotEqualTo(1);
+        assertThat(tabla[1]).isNotEqualTo(2);
+        assertThat(tabla[tabla.length-1] != 6).isTrue();
     }
 
     @Test
@@ -196,12 +199,12 @@ public class UtilesArraysTest {
         int[] tabla = {1, 2, 3, 4, 5, 6};
 
         //do
-        UtilesArrays.invertir(tabla);
+        tabla = UtilesArrays.invertir(tabla);
 
         //Then
         //Comprobamos que la primera y la ultima posicion se han cambiado
-        assertThat(tabla[0] == 6);
-        assertThat(tabla[tabla.length-1] == 1);
+        assertThat(tabla[0]).isEqualTo(6);
+        assertThat(tabla[tabla.length-1] == 1).isTrue();
     }
 
     @Test
@@ -213,29 +216,30 @@ public class UtilesArraysTest {
         System.setOut(new PrintStream(outContent));
         //When
         //declaramos e inicializamos la variable que vamos a imprimir
-        int[] tabla= {1, 2, 3, 4, 5, 6};
+        int[] tabla= {1, 2, 3};
 
         //Llamamos a nuestra funcion
         UtilesArrays.imprimir(tabla);
         //Then
         //Comprobamos que el mensaje se muestra
-        assertThat("\t" + Arrays.toString(tabla) == outContent.toString());
 
+        assertThat(outContent.toString()).startsWith("\t[1, 2, 3]").endsWith("\n");
         System.setOut(originalOut);
+
     }
 
     @Test
     void buscar(){
         //When
-        int[] tabla = {1, 2, 3, 4, 5, 6};
-        int elemento = 3;
+        int[] tabla = {9, 1, 8, 7, 2, 6};
+        int elemento = 1;
 
         //do
         UtilesArrays.buscar(tabla, elemento);
 
         //Then
         //Comprobamos que el elemento encontrado al buscar sea el que le hemos pasado
-        assertThat(UtilesArrays.buscar(tabla, elemento) == 3);
+        assertThat(UtilesArrays.buscar(tabla, elemento)).isEqualTo(1);
 
     }
 
@@ -247,12 +251,12 @@ public class UtilesArraysTest {
         int posicionCorte = 5;
 
         //do
-        UtilesArrays.partirPor(tabla, posicionInicio, posicionCorte);
+        tabla = UtilesArrays.partirPor(tabla, posicionInicio, posicionCorte);
 
         //Then
         //Comprobamos que ahora la posicion inicial es 2 y la ultima 4
-        assertThat(tabla[0] == 2);
-        assertThat(tabla[tabla.length-1] == 4);
+        assertThat(tabla[0]).isEqualTo(2);
+        assertThat(tabla[tabla.length-1]).isEqualTo(5);
 
     }
 
@@ -264,7 +268,8 @@ public class UtilesArraysTest {
 
         //Then
         //Comprobamos que los dos arrays son iguales
-        assertThat(UtilesArrays.sonIguales(tabla1, tabla2));
+        assertThat((tabla1[0])).isEqualTo(tabla2[0]);
+        assertThat((tabla1[tabla1.length-1])).isEqualTo((tabla2[tabla2.length-1]));
     }
 
     @Test
@@ -275,7 +280,7 @@ public class UtilesArraysTest {
 
         //Then
         //Comprobamos que los dos arrays son iguales
-        assertThat(!UtilesArrays.sonIguales(tabla1, tabla2));
+        assertThat(!UtilesArrays.sonIguales(tabla1, tabla2)).isTrue();
     }
 
     @Test
@@ -290,19 +295,23 @@ public class UtilesArraysTest {
 
         //Then
         //Comprobamos que el elemento a comparar en ambas tablas es igual
-        assertThat(UtilesArrays.elementosIguales(tabla1, tabla2, posicionAcomparar) == true);
+        assertThat(UtilesArrays.elementosIguales(tabla1, tabla2, posicionAcomparar)).isTrue();
 
     }
 
     @Test
     void concatenarArrays(){
+        //When
         int[] tabla1 = {1, 2, 3, 4, 5};
         int[] tabla2 = {1, 2, 3, 4, 5};
         int[] tablaUnion = {1, 2, 3, 4, 5, 1, 2, 3, 4, 5};
 
+        //do
         UtilesArrays.concatenarArrays(tabla1, tabla2);
 
-        assertThat(UtilesArrays.concatenarArrays(tabla1, tabla2) == tablaUnion);
+        //Then
+        //Comprobamos que la union de las tablas es igual a la tabla que las engloba
+        assertThat(UtilesArrays.concatenarArrays(tabla1, tabla2)).isEqualTo(tablaUnion);
     }
 
 }
